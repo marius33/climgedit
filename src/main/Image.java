d1150fc49fcb6da23364c1613bf23cf03de3fc7e
marius33 <deviantart33>
2017-02-08 16:23:48 +0200

    non important

--- src/main/Image.java
+++ src/main/Image.java
@@ -1,7 +1,5 @@
 package main;
 
-import org.imgscalr.Scalr;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -21,7 +19,7 @@ public class Image {
 
     private BufferedImage image;
 
-    public Image(){
+    public Image() {
 
     }
 
@@ -29,19 +27,23 @@ public class Image {
         image = ImageIO.read(new File(path));
     }
 
-    public Image(BufferedImage img){
+    public Image(BufferedImage img) {
         image = img;
     }
 
     public void rotate(double theta, RotateMode mode) {
         double radians = Math.toRadians(theta);
 
-        int diagonal = (int) Math.round(Math.sqrt(image.getHeight() * image.getHeight() + image.getWidth() * image.getWidth()));
-        double alpha = Math.asin(image.getWidth()/diagonal);
-        int newWidth = (int) Math.abs(diagonal*Math.cos(alpha+theta));
-        int newHeight = (int) Math.abs(diagonal*Math.sin(alpha+theta));
-        pad((newWidth-image.getWidth())/2, (newHeight-image.getHeight())/2, Color.BLACK);
+        int origWidth = image.getWidth();
+        int origHeight = image.getHeight();
 
+        if (mode.equals(RotateMode.PAD) || mode.equals(RotateMode.PAD_RESIZE)) {
+            int diagonal = (int) Math.round(Math.sqrt(image.getHeight() * image.getHeight() + image.getWidth() * image.getWidth()));
+            double alpha = Math.asin(image.getWidth() / diagonal);
+            int newWidth = (int) Math.abs(diagonal * Math.cos(alpha + theta));
+            int newHeight = (int) Math.abs(diagonal * Math.sin(alpha + theta));
+            pad((newWidth - image.getWidth()) / 2, (newHeight - image.getHeight()) / 2);
+        }
 
         int anchorX = image.getWidth() / 2;
         int anchorY = image.getHeight() / 2;
@@ -49,34 +51,57 @@ public class Image {
         AffineTransformOp afTransfOp = new AffineTransformOp(afTransform, AffineTransformOp.TYPE_BILINEAR);
         image = afTransfOp.filter(image, image);
 
+        if(mode.equals(RotateMode.PAD)){
+            resize(ResizeMode.FIT, origWidth, origHeight);
+        }
 
     }
 
-    public void resize(Scalr.Mode mode, int width, int height) {
-        image = Scalr.resize(image, mode, width, height);
+    public void resize(ResizeMode mode, int width, int height) {
+        AffineTransform afTransform = AffineTransform.getScaleInstance(width/image.getWidth(), height/image.getHeight());
+        AffineTransformOp op = new AffineTransformOp(afTransform, Affine)
     }
 
     public void pad(int left, int right, int top, int bottom, Color c) {
-        int newWidth = left+right+image.getWidth();
-        int newHeight = top+bottom+image.getHeight();
+        int newWidth = left + right + image.getWidth();
+        int newHeight = top + bottom + image.getHeight();
         BufferedImage newImage = new BufferedImage(newWidth, newHeight, image.getType());
-        Graphics g= newImage.getGraphics();
+        Graphics g = newImage.getGraphics();
         g.setColor(c);
         g.drawImage(image, left, top, null);
         g.dispose();
         image = newImage;
     }
 
-    public void pad(int leftRight, int topBottom, Color c){
+    public void pad(int left, int right, int top, int bottom) {
+        pad(left, right, top, bottom, new Color(0));
+    }
+
+    public void pad(int leftRight, int topBottom, Color c) {
         pad(leftRight, leftRight, topBottom, topBottom, c);
     }
 
-    public void pad(int thickness, Color c){
+    public void pad(int leftRight, int topBottom) {
+        pad(leftRight, topBottom, new Color(0));
+    }
+
+    public void pad(int thickness, Color c) {
         pad(thickness, thickness, thickness, thickness, c);
     }
 
+    public void pad(int thickness) {
+        pad(thickness, new Color(0));
+    }
+
     public void crop(int startX, int startY, int width, int height) {
-        image = Scalr.crop(image, startX, startY, width, height);
+        BufferedImage dest = new BufferedImage(width, height, image.getType());
+        Graphics g = dest.getGraphics();
+        g.drawImage(image, 0, 0, width, height,
+                startX, startY, startX + width, startY + height,
+                new Color(0), null);
+        g.dispose();
+        image = dest;
+
     }
 
     public void replaceColors(Color src, Color dst, int threshold) {
@@ -87,13 +112,13 @@ public class Image {
         int[] rgbaSrc = new int[]{src.getRed(), src.getGreen(), src.getBlue(), src.getAlpha()};
         int[] rgbaDest = new int[]{dst.getRed(), dst.getGreen(), dst.getBlue(), dst.getAlpha()};
 
-        Arrays.stream(hNumbers).parallel().forEach(y->{
-            for(int x = image.getWidth()-1; x>=0; x--){
+        Arrays.stream(hNumbers).parallel().forEach(y -> {
+            for (int x = image.getWidth() - 1; x >= 0; x--) {
 
                 int[] channels = new int[4];
                 channels = srcData.getPixel(x, y, channels);
-                for(int i=0; i<4; i++){
-                    if(channels[i]<=(rgbaSrc[i]+threshold) && channels[i]>=(rgbaSrc[i]-threshold))
+                for (int i = 0; i < 4; i++) {
+                    if (channels[i] <= (rgbaSrc[i] + threshold) && channels[i] >= (rgbaSrc[i] - threshold))
                         destData.setPixel(x, y, rgbaDest);
                 }
             }
@@ -102,13 +127,13 @@ public class Image {
         image.setData(destData);
     }
 
-    public void replaceColors(ColorReplacer replacer){
+    public void replaceColors(ColorReplacer replacer) {
         Raster srcData = image.getData();
         WritableRaster destData = srcData.createCompatibleWritableRaster();
         int[] hNumbers = IntStream.range(0, image.getHeight()).toArray();
 
-        Arrays.stream(hNumbers).parallel().forEach(y->{
-            for(int x = image.getWidth()-1; x>=0; x--){
+        Arrays.stream(hNumbers).parallel().forEach(y -> {
+            for (int x = image.getWidth() - 1; x >= 0; x--) {
 
                 int[] channels = new int[4];
                 channels = srcData.getPixel(x, y, channels);
@@ -120,21 +145,38 @@ public class Image {
         image.setData(destData);
     }
 
-    public enum RotateMode{
+    public enum RotateMode {
 
         CROP(0), PAD(1), PAD_RESIZE(2);
 
         private final int v;
 
-        RotateMode(int value){
+        RotateMode(int value) {
             v = value;
         }
 
-        public int getValue(){
+        public int getValue() {
             return v;
         }
 
     }
+
+    public enum ResizeMode {
+
+        FIT_PAD(0), FIT_CROP(1), FIT(2), PAD(3);
+
+        private final int v;
+
+        ResizeMode(int value) {
+            v = value;
+        }
+
+        public int getValue() {
+            return v;
+        }
+
+    }
+
 
     public interface ColorReplacer {
 
